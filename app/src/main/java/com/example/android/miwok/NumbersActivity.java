f96613e2e70703adde1d8015760d14faa37f7589
LuSantanaHolmes <luis.santanaholmes@gmail.com>
2020-03-16 22:15:22 +0100

    add Relative Layouts and correct NumberActivity to NumbersActivity within the whole project

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class NumberActivity extends AppCompatActivity {
+public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_number);
+        setContentView(R.layout.activity_numbers);
     }
 }
